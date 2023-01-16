863e5971d08afc245e8641862352696e99c8f601
Aesthetyx <yixiangtan94@gmail.com>
2023-01-16 22:18:00 +0800

    align files to instructions and updated Readme.txt

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
 from restaurant import views
 
 router = DefaultRouter(trailing_slash=False)
@@ -28,5 +27,4 @@ urlpatterns = [
     path("restaurant/bookings/", include(router.urls)),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
-    path("api-token-auth", obtain_auth_token),
 ]
